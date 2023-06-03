78c758708a0038294dbd9171156044a3a9f3e135
kyawaway <tkmz0216@gmail.com>
2023-06-03 16:59:04 +0900

    visual

---
+++ src/Editor.elm
@@ -0,0 +1,94 @@
+module Editor exposing (..)
+
+
+import Bootstrap.Form.Textarea as Textarea
+import Html exposing (Html, div, text)
+import Html.Attributes as HAttrs exposing (style)
+import Json.Decode as Decode exposing (Decoder)
+
+
+-- Constants
+
+
+initialContent : String
+initialContent =
+    ""
+
+
+-- Types
+
+
+type Msg
+    = TextareaMsg String
+
+
+type alias Model =
+    { code : String
+    , messages : List String
+    }
+
+
+
+-- Initialize
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { code = initialContent
+      , messages = []
+      }
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+updateCode : String -> Model -> Model
+updateCode code model =
+    { model | code = code, messages = "edited" :: model.messages }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        TextareaMsg code ->
+            ( updateCode code model, Cmd.none )
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    Textarea.textarea
+        [ Textarea.id "myarea"
+        , Textarea.attrs
+            [ HAttrs.class "codeEditor"
+
+            , HAttrs.style "color" "#3c372d"
+            , HAttrs.style "border" "none"
+            , HAttrs.style "outline" "none"
+            , HAttrs.style "padding" "20px"
+            , HAttrs.style "height" "100%"
+
+            , HAttrs.style "font-family" "JetBrains Mono, monospace"
+            , HAttrs.style "background" "#fffae6"
+            , HAttrs.style "resize" "none"
+            ]
+        , Textarea.rows 30
+
+        , Textarea.value model.code
+        , Textarea.onInput TextareaMsg
+        ]
+
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,47 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Color
+import Html exposing (Html, text, div )
+import Html.Attributes as HAttrs exposing (style)
+import Html.Events as HEvents
+import Editor as Editor
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
+import Bootstrap.Navbar as Navbar
+import Bootstrap.Button as Button
+import Bootstrap.CDN as CDN
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    {
+        editor : Editor.Model
+        , navbarState : Navbar.State
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+init : () -> ( Model, Cmd Msg )
+init _ =
+    let 
+        ( editorModel, editorCmd ) =
+            Editor.init ()
+        
+        ( navbarState, navbarCmd ) =
+            Navbar.initialState NavbarMsg
+    in
+
+    ( {
+       editor = Editor.updateCode "//input code here" editorModel
+      , navbarState = navbarState
+      }
+      , Cmd.batch
+        [ Cmd.map EditorMsg editorCmd
+        ]
+    )
 
 
 
@@ -22,25 +49,120 @@ init =
 
 
 type Msg
-    = NoOp
+    = EditorMsg Editor.Msg
+    | NavbarMsg Navbar.State
+    | Send
+    | LoadCode
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        EditorMsg editorMsg ->
+            let
+                ( editorModel, editorCmd ) =
+                    Editor.update editorMsg model.editor
+            in
+            ( { model | editor = editorModel }, Cmd.map EditorMsg editorCmd )
+        
+        NavbarMsg state ->
+            ( { model | navbarState = state }, Cmd.none )
+
+        Send ->
+            ( model, Cmd.none ) 
+
+        LoadCode ->
+            ( model, Cmd.none )
+
 
 
 
 ---- VIEW ----
 
+viewNavbar : Model -> Html Msg
+viewNavbar model =
+    let navbarBaseColor = Color.rgb 0.23 0.21 0.18
+        runButtonColor = Color.rgb 0.90 0.76 0.74
+        exampleButtonColor = Color.rgb 0.88 0.94 0.78
+    in
+    Navbar.config NavbarMsg
+        |> Navbar.withAnimation
+        |> Navbar.darkCustom navbarBaseColor
+        |> Navbar.items
+            [ Navbar.itemLink
+                [ HAttrs.style "padding-bottom" "0"
+                , HAttrs.style "padding-top" "0"
+                ]
+                [ Button.button [ Button.primary, Button.onClick <| Send ] [ text "Run" ] ]
+            , Navbar.dropdown
+                { id = "exampleDropdown"
+                , toggle = Navbar.dropdownToggle [] [ text "Examples" ]
+                , items =
+                    [ Navbar.dropdownItem [ HEvents.onClick <| LoadCode ]
+                        [ text "First Example"]
+                    , Navbar.dropdownItem [ HEvents.onClick <| LoadCode ]
+                        [ text "Second Exapmle"]
+                    , Navbar.dropdownItem [ HEvents.onClick <| LoadCode ]
+                        [ text "Third Example." ]
+                    ]
+                }
+            , Navbar.itemLink
+                [ HAttrs.style "padding-bottom" "0"
+                , HAttrs.style "padding-top" "0"
+                ]
+                [ Button.button
+                    [ Button.secondary, Button.attrs [ HEvents.onClick Send ] ]
+                    [ text "About" ]
+                ]
+            ]
+        |> Navbar.view model.navbarState
+
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+    let
+        height100 =
+            HAttrs.style "height" "100%"
 
+        paneMain =
+            [ viewNavbar model
+            , Grid.containerFluid [ HAttrs.style "flex-grow" "1", height100 ]
+                [ Grid.row [ Row.attrs [ height100 ] ]
+                    [ Grid.col [ Col.xs6, Col.attrs [ height100, HAttrs.style "padding" "0" ] ]
+                        [
+                            Html.map EditorMsg <| Editor.view model.editor
+                        ]
+                    , Grid.col
+                        [ Col.xs6
+                        , Col.attrs
+                            [ HAttrs.style "padding" "0"
+                            , HAttrs.style "position" "relative"
+                            , HAttrs.style "flex-grow" "1"
+                            ]
+                        ]
+                        [ div
+                            [ style "position" "fixed"
+                            , style "bottom" "50px"
+                            , style "right" "20px"
+                            , style "width" "45vw"
+                            , style "color" "#5a7323"
+                            ]
+                            [ text <| "result" ]
+                        ]
+                    ]
+                ]
+            ]
+    in
+    div []
+        [ CDN.stylesheet
+        , div
+            [ style "display" "flex"
+            , HAttrs.style "display" "flex"
+            , HAttrs.style "flex-flow" "column"
+            , HAttrs.style "height" "100vh"
+            ]
+            paneMain
+        ]
 
 
 ---- PROGRAM ----
@@ -50,7 +172,7 @@ main : Program () Model Msg
 main =
     Browser.element
         { view = view
-        , init = \_ -> init
+        , init = init
         , update = update
         , subscriptions = always Sub.none
         }
